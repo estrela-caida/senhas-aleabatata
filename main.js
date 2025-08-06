98a594aaacfc5234923c22ab9b2c0f6fe5ea0cd5
estrela-caida <142031549+estrela-caida@users.noreply.github.com>
2025-08-06 08:14:40 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
-const numeroSenha = document.querySelector('.parametro-senha')
+const numeroSenha = document.querySelector('.parametro-senha_texto');
 let tamanhoSenha = 12;
-numeroSenha.textContent = tamanhoSenha
+numeroSenha.textContent = tamanhoSenha;
 
-const botoes = document.querySelectorAll('parametro-senha-botao')
-botoes[0].onclik = diminuiTamanho
-botoes{1}.onclik = aumentaTamanho
+const botoes = document.querySelectorAll('.parametro-senha_botao');
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+function diminuiTamanho(){
+    if (tamanhoSenha > 1){
+        tamanhoSenha--;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
+function aumentaTamanho(){
+    if (tamanhoSenha < 20){
+        tamanhoSenha++;
+    }
+numeroSenha.textContent = tamanhoSenha;
+}
